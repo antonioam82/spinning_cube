adf0dae25216c89293392cf597da18a3382f3169
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-11-30 16:51:17 +0100

    Update spinning_cube.cpp

--- spinning_cube.cpp
+++ spinning_cube.cpp
@@ -11,8 +11,17 @@ using namespace std;
 float A, B, C;
 float cubeWidth = 10;
 int width = 160, height = 44;
+float zBuffer[160 * 44];
 char buffer[160 * 44];
 int backgroundASCIICode = ' ';
+float incrementSpeed;
+float distanceFromCam = 60;
+float K1 = 40;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+int idx;
 
 float calculateX(int i, int j, int k){
     return j * sin(A) * sin(B) * cos(C) - k * cos(A)* sin(B) * cos(C) +
@@ -32,13 +41,33 @@ float calculateZ(int i, int j, int k){
 
 }
 
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch){
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) +distanceFromCam;
+
+    ooz = 1/z;
+    xp = (int)(width/2 + K1 * ooz * x * 2);
+    yp = (int)(height/2 + K1 * ooz * y);
+
+    idx = xp + yp * width;
+}
+
 int main()
 {
     printf("\x1b[2J");
     while(1){
         memset(buffer, backgroundASCIICode, width * height);
-        memset(zBuffer)
+        memset(zBuffer, 0, width * height * 4);
+        for(float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed){
+            for(float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed){
+                    calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+
+
+            }
+        }
     }
 
     return 0;
 }
+
